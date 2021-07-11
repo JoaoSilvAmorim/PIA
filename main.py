ea2a71e8517f9acc6cb2caa2b9c391ea03efdbd8
Victor Amorim <victorsilva6075@gmail.com>
2021-07-10 21:44:24 -0300

    Conclusão

--- main.py
+++ main.py
@@ -1,79 +1,106 @@
 import tensorflow as tf
-
 import numpy as np
 from PIL import Image
 
 
+#Importando o dataset 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-n_train = mnist.train.num_examples # 55,000
-n_validation = mnist.validation.num_examples # 5000
-n_test = mnist.test.num_examples # 10,00
+#Dados do dataset
+treino = mnist.train.num_examples #Imagens para treino
+validacao =  mnist.validation.num_examples #Imagens para validacao
+teste = mnist.test.num_examples #Imagens para teste
 
-n_input = 784   # input layer (28x28 pixels)
-n_hidden1 = 512 # 1st hidden layer
-n_hidden2 = 256 # 2nd hidden layer
-n_hidden3 = 128 # 3rd hidden layer
-n_output = 10   # output layer (0-9 digits)
+entrada = 784 #Camada de entrada, são imagens de 28x28 pixels
+camada1Esc = 512 #Camada oculta 1
+camada2Esc = 256 #Camada oculta 2
+camada3Esc = 128 #Camada oculta 3
+numeroSaida = 10 #Camada de saida
 
 
+#Learningrate, representa o quanto os parâmetros
+#serão ajustados em cada etapa do processo de aprendizado
 learning_rate = 1e-4
-n_iterations = 1000
-batch_size = 128
-dropout = 0.5
 
-X = tf.placeholder("float", [None, n_input])
-Y = tf.placeholder("float", [None, n_output])
-keep_prob = tf.placeholder(tf.float32) 
+#Numero de iteracoes
+numeroIteracoes = 1000
+
+#lote se refere a quantos exemplos de treinamento 
+#estamos usando em cada etapa
+lote = 128
+
+#A variável fora representa um 
+#limiar no qual eliminamos algumas unidades aleatoriamente.
+fora = 0.5
 
 
-weights = {
-    'w1': tf.Variable(tf.truncated_normal([n_input, n_hidden1], stddev=0.1)),
-    'w2': tf.Variable(tf.truncated_normal([n_hidden1, n_hidden2], stddev=0.1)),
-    'w3': tf.Variable(tf.truncated_normal([n_hidden2, n_hidden3], stddev=0.1)),
-    'out': tf.Variable(tf.truncated_normal([n_hidden3, n_output], stddev=0.1)),
+#Para X usamos um formato [None, 784], onde None representa qualquer 
+#quantidade, pois estaremos alimentando em um número indefinido de 
+#imagens de 784 pixels. O formato de Y é [None, 10] pois iremos usá-lo
+#para um número indefinido de saídas de rótulo, com 10 classes possíveis. 
+X = tf.placeholder("float", [None, entrada])
+Y = tf.placeholder("float", [None, numeroSaida])
+guardar = tf.placeholder(tf.float32) 
+
+
+#Aqui são definidos os pesos
+pesos = {
+    'w1': tf.Variable(tf.truncated_normal([entrada, camada1Esc], stddev=0.1)),
+    'w2': tf.Variable(tf.truncated_normal([camada1Esc, camada2Esc], stddev=0.1)),
+    'w3': tf.Variable(tf.truncated_normal([camada2Esc, camada3Esc], stddev=0.1)),
+    'out': tf.Variable(tf.truncated_normal([camada3Esc, numeroSaida], stddev=0.1)),
 }
 
-biases = {
-    'b1': tf.Variable(tf.constant(0.1, shape=[n_hidden1])),
-    'b2': tf.Variable(tf.constant(0.1, shape=[n_hidden2])),
-    'b3': tf.Variable(tf.constant(0.1, shape=[n_hidden3])),
-    'out': tf.Variable(tf.constant(0.1, shape=[n_output]))
+# bias ou tendência, usamos um pequeno valor constante para garantir que os tensores
+# se ativem nos estágios iniciais e, portanto, contribuam para a propagação.
+bias = {
+    'b1': tf.Variable(tf.constant(0.1, shape=[camada1Esc])),
+    'b2': tf.Variable(tf.constant(0.1, shape=[camada2Esc])),
+    'b3': tf.Variable(tf.constant(0.1, shape=[camada3Esc])),
+    'out': tf.Variable(tf.constant(0.1, shape=[numeroSaida]))
 }
 
-layer_1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
-layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
-layer_3 = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
-layer_drop = tf.nn.dropout(layer_3, keep_prob)
-output_layer = tf.matmul(layer_3, weights['out']) + biases['out']
+#Cada camada oculta executará a multiplicação da matriz nas saídas da camada anterior e os pesos da camada
+#atual e adicionará o bias a esses valores. Na última camada oculta, 
+#aplicaremos uma operação de eliminação usando nosso valor keep_prob de 0.5.
+camada1 = tf.add(tf.matmul(X, pesos['w1']), bias['b1'])
+camada2 = tf.add(tf.matmul(camada1, pesos['w2']), bias['b2'])
+camada3 = tf.add(tf.matmul(camada2, pesos['w3']), bias['b3'])
+camaadaSolta = tf.nn.dropout(camada3, guardar)
+camadaSaida = tf.matmul(camada3, pesos['out']) + bias['out']
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=output_layer))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#Entropia cruzada, também conhecida como log-loss, que quantifica a diferença entre duas distribuições de probabilidade
+entropia = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=camadaSaida))
+treinoP = tf.train.AdamOptimizer(1e-4).minimize(entropia)
 
-correct_pred = tf.equal(tf.argmax(output_layer, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+predicaoCorreta = tf.equal(tf.argmax(camadaSaida, 1), tf.argmax(Y, 1))
+acuracia = tf.reduce_mean(tf.cast(predicaoCorreta, tf.float32))
+
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-# train on mini batches
-for i in range(n_iterations):
-    batch_x, batch_y = mnist.train.next_batch(batch_size)
-    sess.run(train_step, feed_dict={X: batch_x, Y: batch_y, keep_prob:dropout})
+#Exibindo acuracia
+for i in range(numeroIteracoes):
+    batch_x, batch_y = mnist.train.next_batch(lote)
+    sess.run(treinoP, feed_dict={X: batch_x, Y: batch_y, guardar:fora})
 
-    # print loss and accuracy (per minibatch)
     if i%100==0:
-        minibatch_loss, minibatch_accuracy = sess.run([cross_entropy, accuracy], feed_dict={X: batch_x, Y: batch_y, keep_prob:1.0})
+        minibatch_loss, minibatch_accuracy = sess.run([entropia, acuracia], feed_dict={X: batch_x, Y: batch_y, guardar:1.0})
         print("Iteração", str(i), "\t| Perdido =", str(minibatch_loss), "\t| Val =", str(minibatch_accuracy))
 
 
-test_accuracy = sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob:1.0})
-print("\Valor aproximado do teste:", test_accuracy)
-
-img = np.invert(Image.open("test_img.png").convert('L')).ravel()
 
 
-prediction = sess.run(tf.argmax(output_layer,1), feed_dict={X: [img]})
-print ("A imagem corresponde ao número:", np.squeeze(prediction))
+testeAcuracia = sess.run(acuracia, feed_dict={X: mnist.test.images, Y: mnist.test.labels, guardar:1.0})
+print("\Valor aproximado do teste:", testeAcuracia)
+
+img = np.invert(Image.open("oito.png").convert('L')).ravel()
+
+prediction = sess.run(tf.argmax(camadaSaida,1), feed_dict={X: [img]})
+
+print ("A imagem é", np.squeeze(prediction))
+
+
